e3a013c69c561bf2ab0bce4245212304dbf5d983
CJ28M <marin.christopher128@gmail.com>
2025-04-07 09:22:07 -0700

    solved UR

--- other/dice.js
+++
@@ -1,7 +0,0 @@
-function generateNumbers() {
-  let slots = document.querySelectorAll(".slot");
-  slots.forEach((slot) => {
-    let randomNumber = Math.random() < 0.5 ? 0 : 1; // Generates either 0 or 1
-    slot.textContent = randomNumber;
-  });
-}
